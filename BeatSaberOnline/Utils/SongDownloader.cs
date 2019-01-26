704e453313c4ff0faf29ab30bc907e363060b60f
brian91292 <8050523+brian91292@users.noreply.github.com>
2019-01-26 01:33:21 -0600

    implement song downloading

--- BeatSaberOnline/Utils/SongDownloader.cs
+++ BeatSaberOnline/Utils/SongDownloader.cs
@@ -38,31 +38,128 @@ namespace BeatSaberOnline.Utils
                 _instance = value;
             }
         }
-        
 
-        public void DownloadSong(string levelId)
+        public static void MoveFilesRecursively(DirectoryInfo source, DirectoryInfo target)
         {
-            try
-            {
-                if (levelId.Length > 32) { levelId = levelId.Substring(0, 32); }
-                BeatSaverDownloader.Misc.SongDownloader.Instance.songDownloaded += downloadedSong;
-                BeatSaverDownloader.Misc.SongDownloader.Instance.RequestSongByLevelID(levelId, (song) =>
-                {
-                    CurrentlyDownloadingSong = song;
-                    if (!BeatSaverDownloader.Misc.SongDownloader.Instance.IsSongDownloaded(CurrentlyDownloadingSong))
-                    {
-                        Data.Logger.Info("Starting to download " + levelId);
-                        StartCoroutine(BeatSaverDownloader.Misc.SongDownloader.Instance.DownloadSongCoroutine(CurrentlyDownloadingSong));
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                MoveFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+            foreach (FileInfo file in source.GetFiles())
+                file.MoveTo(Path.Combine(target.FullName, file.Name));
+        }
 
-                    }
-                    else
-                    {
-                        Data.Logger.Info("Song is already downloaded");
-                    }
-                });
-            } catch (Exception e)
+        public static void EmptyDirectory(string directory, bool delete = true)
+        {
+            if (Directory.Exists(directory))
             {
-                Data.Logger.Error(e);
+                var directoryInfo = new DirectoryInfo(directory);
+                foreach (System.IO.FileInfo file in directoryInfo.GetFiles()) file.Delete();
+                foreach (System.IO.DirectoryInfo subDirectory in directoryInfo.GetDirectories()) subDirectory.Delete(true);
+
+                if (delete) Directory.Delete(directory);
+            }
+        }
+
+        public static IEnumerator ExtractZip(string zipPath, string extractPath)
+        {
+            if (File.Exists(zipPath))
+            {
+                bool extracted = false;
+                try
+                {
+                    ZipFile.ExtractToDirectory(zipPath, ".mpdownloadcache");
+                    extracted = true;
+                }
+                catch (Exception)
+                {
+                    Data.Logger.Info($"An error occured while trying to extract \"{zipPath}\"!");
+                    yield break;
+                }
+
+                yield return new WaitForSeconds(0.25f);
+
+                File.Delete(zipPath);
+
+                try
+                {
+                    if (extracted)
+                    {
+                        if (!Directory.Exists(extractPath))
+                            Directory.CreateDirectory(extractPath);
+
+                        MoveFilesRecursively(new DirectoryInfo($"{Environment.CurrentDirectory}\\.mpdownloadcache"), new DirectoryInfo(extractPath));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Data.Logger.Info($"An exception occured while trying to move files into their final directory! {e.ToString()}");
+                }
+            }
+        }
+
+        public static IEnumerator DownloadFile(string url, string path)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get(url))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Data.Logger.Info($"Http request error! {www.error}");
+                    yield break;
+                }
+                Data.Logger.Info($"Success downloading \"{url}\"");
+                byte[] data = www.downloadHandler.data;
+                try
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(path)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+                    File.WriteAllBytes(path, data);
+                    Data.Logger.Info("Downloaded file!");
+                }
+                catch (Exception)
+                {
+                    Data.Logger.Info("Failed to download file!");
+                    yield break;
+                }
+            }
+        }
+
+        public IEnumerator DownloadSong(string levelId, Action songDownloaded)
+        {
+            if (levelId.Length > 32) { levelId = levelId.Substring(0, 32); }
+            
+            using (UnityWebRequest www = UnityWebRequest.Get($"https://beatsaver.com/api/songs/search/hash/{levelId}"))
+            {
+                yield return www.SendWebRequest();
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Data.Logger.Info(www.error);
+                    yield break;
+                }
+                
+                JSONNode result = JSON.Parse(www.downloadHandler.text);
+                if (result["total"].AsInt == 0) yield break;
+
+                foreach (JSONObject song in result["songs"].AsArray)
+                {
+                    string currentHash = song["hashMd5"].Value.ToLower();
+                    if (currentHash == levelId.ToLower())
+                    {
+                        EmptyDirectory(".mpdownloadcache");
+
+                        string zipPath = Path.Combine(Environment.CurrentDirectory, ".mpdownloadcache", $"{song["version"].Value}.zip");
+                        Data.Logger.Info($"ZipPath: {zipPath}");
+                        yield return DownloadFile(song["downloadUrl"].Value, zipPath);
+                        yield return ExtractZip(zipPath, Path.Combine(Environment.CurrentDirectory, "CustomSongs", song["version"].Value));
+
+                        SongLoader.Instance.RefreshSongs(false);
+                        EmptyDirectory(".mpdownloadcache", true);
+                        
+                        songDownloaded?.Invoke();
+                        break;
+                    }
+                }
             }
         }
 
